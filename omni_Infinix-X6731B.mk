9a9e185c2a8623d0bb8018386e52b71512037ea4
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-03-23 15:18:24 +0100

    Infinix-X6731B: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: infinix Model: Infinix X6731B Script version: 3.0.0

---
+++ omni_Infinix-X6731B.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from Infinix-X6731B device
+$(call inherit-product, device/infinix/Infinix-X6731B/device.mk)
+
+PRODUCT_DEVICE := Infinix-X6731B
+PRODUCT_NAME := omni_Infinix-X6731B
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix X6731B
+PRODUCT_MANUFACTURER := infinix
+
+PRODUCT_GMS_CLIENTID_BASE := android-infinix
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vext_x6731b_h896-user 12 SP1A.210812.016 525606 release-keys"
+
+BUILD_FINGERPRINT := Infinix/X6731B-OP/Infinix-X6731B:12/SP1A.210812.016/240117V1442:user/release-keys
